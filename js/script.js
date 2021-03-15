344988bf7b3080262df81029ab09ab5f966f58ef
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-15 13:38:58 +0100

    final update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 function mineField(){
 
   var comNums = [];
-  var numsLength = 5;
+  var numsLength = 16;
   var comNumMin = 1;
-  var comNumMax = 10;
+  var comNumMax = 100;
+
+  // Bonus scelta difficoltà
+
+  // var difficulty = ['facile', 'medio', 'difficile'];
+  //
+  // do {
+  //   var userChoise = prompt('Scegli una difficoltà (facile, medio o difficile)');
+  //
+  //   if (!difficulty.includes(userChoise)){
+  //     alert('Scelta non valida');
+  //   }
+  // } while (!difficulty.includes(userChoise));
+  //
+  // if (userChoise == difficulty[0]){
+  //   comNumMax = 50;
+  // }
+  //
+  // if (userChoise == difficulty[1]){
+  //   comNumMax = 80;
+  // }
+  //
+  // if (userChoise == difficulty[2]){
+  //   comNumMax = 100;
+  // }
 
   do {
     var comNum = getRnd(comNumMin, comNumMax);
@@ -21,23 +45,19 @@ function mineField(){
   do {
     var userNum = parseInt(prompt('inserisci un numero tra 1 e 100'));
 
-    console.log(userNum);
-
-    console.log('round', round);
-
     if (userNum > comNumMax || userNum < comNumMin) {
       alert('Numero inserito non compreso tra 1 e 100');
     }
 
     if (userNums.includes(userNum)){
-      alert('Numero già inserito');
+      alert('Scelta non valida o numero già inserito');
     } else {
       userNums.push(userNum);
       round++;
     }
 
     if (comNums.includes(userNum)){
-      console.log('hai perso');
+      console.log('Hai perso!');
       userControl = false;
     }
   } while (userControl && (round != comNumMax - numsLength));
